243aadbef8160216715d35b50fe214b9134593af
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2019-11-12 18:27:48 +0300

    Header Started

--- assets/src/script/custom/app.js
+++ assets/src/script/custom/app.js
@@ -1,3 +1,17 @@
+/* Head Blur Tab Page Title */
+$(function(){
+    var $pageHeadTagTitle       = $('title');
+    var $pageHeadTagTitleText   = $pageHeadTagTitle.text();
+
+    $(window).focus(function(){
+        $pageHeadTagTitle.text($pageHeadTagTitleText)
+    });
+
+    $(window).blur(function(){
+        $pageHeadTagTitle.text('E-TRADE')
+    });
+});
+/* Head Blur Tab Page Title */
 
 /* Loading Info */
 console.log("Custom App Javascript Loading Successful");
