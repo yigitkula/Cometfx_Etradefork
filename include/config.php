98fa8e3760036389aa7643711abe9fef16ce37df
Yigit Kula <yigitkula@gmail.com>
2019-12-04 09:53:05 +0300

    Forms & Captcha Fix

---
+++ include/config.php
@@ -0,0 +1,11 @@
+<?php
+    $SMTP_HOST = 'smtp.yandex.com.tr';
+    $SMTP_PORT = 465;
+    $MAIL_USERNAME = 'eyigitkula@yandex.com';
+    $MAIL_PASSWORD = 'Mad(6242)MchnK';
+    $MAIL_FROM = 'eyigitkula@yandex.com';
+    $MAIL_FROM_NAME = 'E-trade.com';
+    $MAIL_TO = 'yigit.kula@icrypex.com';
+    $CAPTCHA_ENABLED = true;
+    $CAPTCHA_SECRET = '6LdipsUUAAAAAICd2zm6R20GDbbgqH6sj1JZ_LKJ';
+?>
